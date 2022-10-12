8dc69e6a8057efd2acac10c619a3f442d6d9613c
Snerzel <snerzel@github.com>
2022-10-12 00:04:30 -0700

    added like and delete buttons

--- breed.js
+++ breed.js
@@ -6,6 +6,7 @@ class Breed {
         this.image_url = breedAttributes.image_url
         this.info = breedAttributes.info
         this.group = breedAttributes.group
+        this.likes = breedAttributes.likes
         Breed.all.push(this)
     }
 
@@ -14,16 +15,20 @@ class Breed {
 renderBreeds() {
     return `
                 <div data-id=${this.id}> 
-                <img src=${this.image_url} height="400" width="600">
+                    <img src=${this.image_url} height="400" width="600">
                     <h3>${this.name}</h3>
                     <p>${this.group.name}</p>
                     <p>${this.info}</p>
-                   
+                    <p>Likes: ${this.likes}</p>
+                    <button class="like-btn">Like</button>
+                    <button class="delete-btn">Delete</button>
                 </div>
                 <br></br>`;
 
                
 }
+
+
 
 }
 Breed.all = [];
--- index.js
+++ index.js
@@ -1,27 +1,34 @@
 const endPoint = "http://localhost:3000/api/v1/breeds"
 
 document.addEventListener('DOMContentLoaded', () => {
+    console.log("a")
     getBreeds()
-
+    console.log("b")
     const addBreedForm = document.querySelector("#add-breed-form")
 
     addBreedForm.addEventListener("submit", (e) => addBreedHandler(e))
 })
 
 function getBreeds() {
+    console.log("c")
     fetch(endPoint)
     .then(response => response.json())
     .then(breeds => {
+        console.log(breeds.data)
         breeds.data.forEach(breed => {
-
+            console.log("d")
+            
             let newBreed = new Breed(breed, breed.attributes)
+            
+           
             document.querySelector('#breed-container').innerHTML += newBreed.renderBreeds();
-            // renderBreeds(breed)
+            
                 
 
         })
-
+    
     })
+    console.log("e")
 }
 
 function addBreedHandler(e) {
@@ -36,8 +43,9 @@ function addBreedHandler(e) {
 
 
 
-function postBreed(name, image_url, info, group_id) {
-    console.log(name, image_url, info, group_id)
+
+function postBreed(name, image_url, info, group_id, likes) {
+    console.log(name, image_url, info, group_id, likes)
     fetch(endPoint, {
         method: "POST",
         headers: {
@@ -48,7 +56,8 @@ function postBreed(name, image_url, info, group_id) {
             name: name,
             image_url: image_url,
             info: info,
-            group_id: group_id
+            group_id: group_id,
+            likes: 0
         })
     })
     .then(response => response.json())
